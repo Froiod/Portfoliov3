44a8718bf0c702bf576dd7ffd5742160ddbdfd81
Paolo Guray <froiodpaolo@gmail.com>
2023-02-28 14:12:28 +0800

    skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import html from '../assets/html.svg'
+import css from '../assets/css.svg'
+import js from '../assets/js.svg'
+import react from '../assets/react.svg'
+import bootstrap from '../assets/bootstrap.svg'
+import tailwind from '../assets/tailwindcss.svg'
+import postgress from '../assets/postgresql.svg'
+
+const Skills = () => {
+  return (
+    <section className='mx-12 mb-12 border-8 border-gray-900'>
+      <div className=''>
+        <h2 className='text-2xl text-blue-100 font-semibold bg-gray-900 inline-block pb-1 pr-1 rounded-br-xl'>
+          Languages:
+        </h2>
+        <div className='grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4 space-y-2 items-center py-4'>
+          <div className='flex flex-col justify-center items-center'>
+            <img src={html} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>HTML</h3>
+          </div>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={css} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>CSS</h3>
+          </div>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={js} alt="" className='w-[36%]'/>
+            <h3 className='text-xl text-center font-semibold'>JavaScript</h3>
+          </div>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={react} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>SQL</h3>
+          </div>
+        </div>
+      </div>
+      <div>
+      <h2 className='text-2xl text-blue-100 font-semibold bg-gray-900 inline-block pb-1 pr-1 rounded-br-xl'>
+          Techs:
+        </h2>
+        <div className='grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4  space-y-2 items-center py-4'>
+          <div className='flex flex-col justify-center items-center'>
+            <img src={bootstrap} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>Bootstrap</h3>
+          </div>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={tailwind} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>Tailwind</h3>
+          </div>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={react} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>ReactJS</h3>
+          </div>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={postgress} alt="" className='w-[40%]'/>
+            <h3 className='text-xl text-center font-semibold'>PostgreSQL</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
